e96c112a5322e7e9a625c2458e3cb65436c7b047
Beuche <76558020+Beuche@users.noreply.github.com>
2023-12-16 17:48:17 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -28,8 +28,9 @@ radio.setGroup(kanal)
 basic.showString("" + (anzeige_holen(kanal)))
 let motor_rechts = 0
 let motor_links = 0
-let faktor = 0.5
+let faktor = 4
 let blinkfarbe = true
+let pause2 = 0
 loops.everyInterval(500, function () {
     if (blinkfarbe) {
         calliBot2.setRgbLed1(C2RgbLed.All, 0xff0000, 8)
@@ -39,26 +40,36 @@ loops.everyInterval(500, function () {
     blinkfarbe = !(blinkfarbe)
 })
 basic.forever(function () {
-    motor_links = (x + y) * -1
-    motor_rechts = (y - x) * -1
-    if (x >= 0) {
-        x = Math.sqrt(Math.abs(x)) * faktor
-    } else {
-        x = Math.sqrt(Math.abs(x)) * faktor * -1
+    if (calliBot2.readBumperSensor(C2Sensor.rechts, C2State.an) || calliBot2.readBumperSensor(C2Sensor.links, C2State.an)) {
+        pause2 = input.runningTime() + 1000
+        calliBot2.motor(C2Motor.beide, C2Dir.rueckwaerts, 31)
+        basic.pause(500)
+        calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
     }
-    if (y >= 0) {
-        y = Math.sqrt(Math.abs(y)) * faktor
+    if ((Math.abs(x) > 1 || Math.abs(y) > 1) && input.runningTime() > pause2) {
+        if (x >= 0) {
+            x = Math.sqrt(Math.abs(x)) * faktor
+        } else {
+            x = Math.sqrt(Math.abs(x)) * faktor * -1
+        }
+        if (y >= 0) {
+            y = Math.sqrt(Math.abs(y)) * faktor
+        } else {
+            y = Math.sqrt(Math.abs(y)) * faktor * -1
+        }
+        motor_links = (x + y) * -1
+        motor_rechts = (y - x) * -1
+        if (motor_links >= 0) {
+            calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, motor_links)
+        } else {
+            calliBot2.motor(C2Motor.links, C2Dir.rueckwaerts, Math.abs(motor_rechts))
+        }
+        if (motor_rechts >= 0) {
+            calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, motor_rechts)
+        } else {
+            calliBot2.motor(C2Motor.rechts, C2Dir.rueckwaerts, Math.abs(motor_links))
+        }
     } else {
-        y = Math.sqrt(Math.abs(y)) * faktor * -1
-    }
-    if (motor_links >= 0) {
-        calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, motor_links)
-    } else {
-        calliBot2.motor(C2Motor.links, C2Dir.rueckwaerts, Math.abs(motor_rechts))
-    }
-    if (motor_rechts >= 0) {
-        calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, motor_rechts)
-    } else {
-        calliBot2.motor(C2Motor.rechts, C2Dir.rueckwaerts, Math.abs(motor_links))
+        calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
     }
 })
